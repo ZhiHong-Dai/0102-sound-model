0db81c3166666d60c1c8f6910dd54418235d1b97
ZhiHong-Dai <61874446+ZhiHong-Dai@users.noreply.github.com>
2021-01-02 14:05:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://zhihong-dai.github.io/0102-sound-model/model.json';
 
 
 function preload() {
